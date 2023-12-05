1f9b225f1361d4733a842b8ffc1e7f5415f094e0
Gauchinho0 <145671971+Gauchinho0@users.noreply.github.com>
2023-12-04 23:29:27 -0300

    Update calc_faculade.py

--- codigos_legais_python/calc_faculade.py
+++ codigos_legais_python/calc_faculade.py
@@ -1,22 +1,45 @@
-print ('\nCalculadora nota de Provas \n') 
-
-#Função de verificação_numero
-#Caso o usuario insira um caracter que nao seja um numero e dentro do raio de (10,0)
-#Retorna sempre a classe errada mantendo a anterior "GA" e "GB"
+print("\nCalculadora nota de Provas (soma nota total e nota media para tirar media)\n")
 
 def verificar_numero(nota_nome):
     while True:
         try:
-            num = float(input(f'Digite nota {nota_nome}: '))
+            num = float(input(f'\nDigite nota {nota_nome}: '))
             if 0 <= num <= 10:
                 return num
             else:
-                print(f"Por favor, insira a nota {nota_nome} entre 10 e 0.")
+                print(f"\nPor favor, insira a nota {nota_nome} entre 10 e 0.")
         except ValueError:
-            print(f"Por favor, insira um número válido para {nota_nome}.")
+            print(f"\nPor favor, insira um número válido para {nota_nome}.")
 
-num1 = verificar_numero("Grau A")
-num2 = verificar_numero("Grau B")
+# Loop para garantir escolha válida
+escolha = ""
+while escolha.lower() not in ["soma", "media"]:
+    escolha = input("Escolha a opção (soma / media): ")
+    if escolha.lower() not in ["soma", "media"]:
+        print("\nEscolha inválida. Por favor, escolha entre as opções 'soma' ou 'media'.\n")
 
-nota_final = num1 * 0.3 + num2 * 0.7
-print('\nNota Grade Final:', nota_final,'\n')
+if escolha.lower() == "soma":
+    # Calculando a soma das notas GA e GB
+    nota_ga = verificar_numero("GA")
+    nota_gb = verificar_numero("GB")  # Recebe a nota de GB diretamente
+    
+    nota_final = nota_ga * 0.3 + nota_gb * 0.7
+    
+    # Exibindo o resultado final da soma
+    print(f'\nO resultado final da soma é: {nota_final:.2f}\n')
+
+elif escolha.lower() == "media":
+    # Informando a nota de GA que já foi obtida
+    nota_ga = verificar_numero("GA") * 0.3
+    
+    # Calculando o quanto falta na nota de GB para atingir a média 6
+    nota_gb_faltando = (6 - nota_ga) / 0.7
+    
+    # Exibindo o quanto falta na nota de GB para atingir a média 6
+    print(f'\nVocê precisa tirar pelo menos na prova {nota_gb_faltando:.2f} da nota da prova GB (nota Bruta) para atingir a média 6.')
+
+    # A nota poderada é a notaGB ja multiplicada pelo 0.7 multiplicado
+    nota_gb_ponderada = nota_gb_faltando * 0.7
+
+    # Exibindo o resultado ponderado de GB usando a média 6
+    print(f'O resultado ponderado (GBx07) de GB usando a média 6 é: {nota_gb_ponderada:.2f}\n')
