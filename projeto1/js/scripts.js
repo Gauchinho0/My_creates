7242fbd1955cd2296decadfa60e3a6118964db4b
Gauchinho0 <145671971+Gauchinho0@users.noreply.github.com>
2024-06-18 18:37:39 -0300

    Last update
    Projeto finalizado

--- projeto1/js/scripts.js
+++ projeto1/js/scripts.js
@@ -5,7 +5,13 @@ $(document).ready(function() {
         var weight = parseFloat($('#bmi-weight').val());
         var height = parseFloat($('#bmi-height').val()) / 100; // Convertendo para metros
         var bmi = weight / (height * height);
-        $('#bmi-result').html('Seu IMC: ' + bmi.toFixed(2));
+
+        // Verifica se o peso está dentro do intervalo desejado
+        if (weight > 0 && weight <= 1000) {
+            $('#bmi-result').html('Seu IMC: ' + bmi.toFixed(2));
+        } else {
+            $('#bmi-result').html('Peso deve ser maior que 0 e menor ou igual a 1000 kg');
+        }
     });
 
     // Calculadora de TMB
@@ -17,12 +23,16 @@ $(document).ready(function() {
         var gender = $('#bmr-gender').val();
         var bmr;
 
-        if (gender === 'male') {
-            bmr = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age);
+        // Verifica se o peso está dentro do intervalo desejado
+        if (weight > 0 && weight <= 1000) {
+            if (gender === 'male') {
+                bmr = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age);
+            } else {
+                bmr = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
+            }
+            $('#bmr-result').html('Seu TMB: ' + bmr.toFixed(2) + ' calorias/dia');
         } else {
-            bmr = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
+            $('#bmr-result').html('Peso deve ser maior que 0 e menor ou igual a 1000 kg');
         }
-
-        $('#bmr-result').html('Seu TMB: ' + bmr.toFixed(2) + ' calorias/dia');
     });
 });
